e79eea7b584372740c48130a67ed4185f0de5e23
DanCouper <danielcouper81@googlemail.com>
2023-03-05 16:26:30 +0000

    complete assembly-line, d/l structured logs

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -1,7 +1,14 @@
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {speed}")
+    let total = (speed as f64) * 221.0;
+    match speed {
+        0 => 0.0,
+        1..=4 => total,
+        5..=8 => 0.9 * total,
+        9..=10 => 0.77 * total,
+        _ => panic!("assembly line production speed *must* be between 0 and 10 (inclusive)"),
+    }
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {speed}")
+    (production_rate_per_hour(speed) as u32) / 60
 }
